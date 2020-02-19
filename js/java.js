d772b5f21677aff303721ed5ee7f732af35aec3e
DESSE Felicite Maryvonne <desse.maryvonne@gmail.com>
2020-02-19 08:03:37 +0000

    barre de recherche

---
+++ js/java.js
@@ -0,0 +1,30 @@
+// var doc =  document.getElementsByClassName("recherche");
+// var word = "foo", queue = [document.body], curr;
+// while(curr = queue.pop()){
+//     if(!curr.textContent.match(woed))
+//         continue;
+//     for(var i = 0; i < curr.childNodes.length;i++){
+//         switch(curr.childNodes[i].nodeType){
+//             case Node.TEXT_NODE:
+//                 if(curr.childNodes[i].textContent.match(word)){
+//                     console.log("FOUND!");
+//                     console.log(curr)
+//                 }
+//                 break;
+//             case Node.ELEMENT_NODE:
+//             queue.push(curr.childNodes[i])
+//         }
+//     }
+// }
+findText(node,text){
+    if(node.childNodes.legnth==0){
+        if(node.textContent.indexOf(text)==-1)
+            return[];
+        return [node];
+    }
+    var matchingNodes = new Array();
+    for(child in node.childNodes){
+        matchingNodes.concat(findText(child, text));
+    }
+    return matchingNodes;
+}
